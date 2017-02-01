db5743f89e38b5013192bf7050be71d86cb2eaee
intelfike <intelfike@gmail.com>
2017-02-01 10:32:53 +0900

    機能追加

--- ringbuf/singletype.go
+++ ringbuf/singletype.go
@@ -5,39 +5,70 @@ import (
 	"reflect"
 )
 
-type singleRingBuf struct {
+type SingleRingBuf struct {
 	Val   reflect.Value
 	Index int
 }
 
-// Exsample
+// Exsample:
 // NewSingleType(make([]int, 22))
-func NewSingleType(i interface{}) singleRingBuf {
-	mrb := *new(singleRingBuf)
-	mrb.Val = reflect.ValueOf(i)
+func NewSingleType(i interface{}) SingleRingBuf {
+	srb := *new(SingleRingBuf)
+	srb.Val = reflect.ValueOf(i)
 
-	if mrb.Val.Kind() != reflect.Slice {
+	if srb.Val.Kind() != reflect.Slice {
 		panic("Argument of NewSingleType({}interface) is Slice only: Exsample -> make([]int, 2)")
 	}
-	return mrb
+	return srb
 }
 
-// write to ring buffer
-func (m *singleRingBuf) Write(i interface{}) {
-	m.Val.Index(m.Index).Set(reflect.ValueOf(i))
-	m.Index++
-	m.Index %= m.Val.Len()
+// write to ring buffer.
+func (s *SingleRingBuf) Write(i interface{}) {
+	s.Val.Index(s.Index).Set(reflect.ValueOf(i))
+	s.Index++
+	s.Index %= s.Val.Len()
 }
 
-// get slice in interface{}
-// type assert is yourself
-func (m *singleRingBuf) Get() interface{} {
-	start := m.Val.Slice(m.Index, m.Val.Len())
-	end := m.Val.Slice(0, m.Index)
+// Get older element.
+// 0 is the most old.
+func (s *SingleRingBuf) IndexOld(i int) interface{} {
+	index := s.LoopModLen(i + s.Index)
+	return s.Val.Index(index)
+}
+
+// Get newer element.
+// 0 is the most new.
+func (s *SingleRingBuf) IndexNew(i int) interface{} {
+	return s.IndexOld(-1 - i)
+}
+
+// Get slice in interface{}.
+// Type assert is yourself.
+func (s *SingleRingBuf) Get() interface{} {
+	start := s.Val.Slice(s.Index, s.Val.Len())
+	end := s.Val.Slice(0, s.Index)
 	return reflect.AppendSlice(start, end).Interface()
 }
 
-// Get() to string
-func (m singleRingBuf) String() string {
-	return fmt.Sprint(m.Get())
+// Get() to string.
+func (s SingleRingBuf) String() string {
+	return fmt.Sprint(s.Get())
+}
+
+// -1 % 3 != -1
+// -1 % 3 == 2
+func LoopMod(i, j int) int {
+	mod := i % j
+	if mod < 0 {
+		mod += j
+	}
+	return mod
+}
+
+func (s SingleRingBuf) LoopModLen(i int) int {
+	return LoopMod(i, s.Val.Len())
+}
+
+func (s SingleRingBuf) Len() int {
+	return s.Val.Len()
 }
